f6846c437ba19c550991092a44ab5fc6dea36383
MahendraMadavali <mahendra.madavali.developer@gmail.com>
2025-08-06 21:31:07 +0530

    Initial commit

---
+++ app.js
@@ -0,0 +1,7 @@
+const obj = require('./sum.js')
+
+const {a, b, calculateSum} = obj;
+
+console.log("helllo mahendra")
+console.log(a, b)
+calculateSum(2,3)
---
+++ sum.js
@@ -0,0 +1,10 @@
+let a =10
+
+let b=20
+
+function calculateSum(a,b){
+    console.log(a+b)
+}
+
+
+module.exports ={a:a, b:b, calculateSum: calculateSum};
